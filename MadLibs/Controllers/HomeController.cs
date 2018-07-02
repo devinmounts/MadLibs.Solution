cd1d0c9276c97440d5d145c11182bdbed380b89a
Devin Mounts and Renee Sarley <devin@>
2018-07-02 16:28:17 -0700

    madlib mvc

---
+++ MadLibs/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MadLibs.Models;
+
+namespace MadLibs.Controllers
+{
+    public class HomeController : Controller
+    {
+        //[Route("/")]
+        //public ActionResult FinalMadLib()
+        //{
+
+        //    MadLibVariable test = new MadLibVariable();     
+        //    return View(test);
+        //}
+
+        [Route("/")]
+        public ActionResult Form()
+        {
+            return View();
+        }
+
+        [Route("/mad_action")]
+        public ActionResult MadAction()
+        {
+            MadLibVariable newMadLib = new MadLibVariable();
+            newMadLib.SetName(Request.Query["name"]);
+            newMadLib.SetPlace(Request.Query["place"]);
+            newMadLib.SetVerb(Request.Query["verb"]);
+            return View("FinalMadLib", newMadLib);
+        }
+    }
+}
---
+++ MadLibs/Models/MadLibVariable.cs
@@ -0,0 +1,40 @@
+﻿using System;
+namespace MadLibs.Models
+{
+    public class MadLibVariable
+    {
+        private string _verb;
+        private string _name;
+        private string _place;
+
+        public string GetVerb()
+        {
+            return _verb;
+        }
+
+        public void SetVerb(string newVerb)
+        {
+            _verb = newVerb;
+        }
+
+        public string GetName()
+        {
+            return _name;
+        }
+
+        public void SetName(string newName)
+        {
+            _name = newName;
+        }
+
+        public string GetPlace()
+        {
+            return _place;
+        }
+
+        public void SetPlace(string newPlace)
+        {
+            _place = newPlace;
+        }
+    }
+}
